4a97750b4407971d877e02cf65723a30e9e5c3c8
Eric Valera Miller <eric@elericuz.com>
2022-12-11 13:05:30 -0500

    Initial commit

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+const { initializeApp } = require('firebase/app');
+const { getAnalytics } = require('firebase/analytics');
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
