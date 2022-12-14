80ac19cd2bc5417805fa22a25fe860fa073cbab3
Eric Valera Miller <eric@elericuz.com>
2022-12-13 19:10:43 -0500

    dashboard and transactions

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -2,7 +2,6 @@ const _ = require('lodash');
 const moment = require('moment');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const User = require('../models/users');
 
 exports.signup = (req, res, next) => {
 
